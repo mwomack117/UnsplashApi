48f41ea42e0d1f8199c1e473b0c1aecc76c33823
mwomack <michael.womack84@gmail.com>
2019-08-27 15:18:34 -0700

    Updates

---
+++ precache-manifest.85eaeaba82a34154d5c587a74ac0237a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8bf2e30e5e51d7fab2d2c4be7a532a",
+    "url": "/UnsplashApi/index.html"
+  },
+  {
+    "revision": "7e7268777be2626b1e16",
+    "url": "/UnsplashApi/static/css/main.ae6466ce.chunk.css"
+  },
+  {
+    "revision": "97f81f4095946ed53456",
+    "url": "/UnsplashApi/static/js/2.136894e0.chunk.js"
+  },
+  {
+    "revision": "7e7268777be2626b1e16",
+    "url": "/UnsplashApi/static/js/main.ecaea040.chunk.js"
+  },
+  {
+    "revision": "cdcd3dbe07137a9ba8cb",
+    "url": "/UnsplashApi/static/js/runtime~main.105ec15c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Unsplash-Api/precache-manifest.8915f66a9248eeb4f312881d6c90fca0.js"
+  "/UnsplashApi/precache-manifest.85eaeaba82a34154d5c587a74ac0237a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Unsplash-Api/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/UnsplashApi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
