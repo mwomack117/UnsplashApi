6a6de9f8f3bb8cbf7387d0387e8db01c73ac69ad
mwomack <michael.womack84@gmail.com>
2019-08-27 15:08:32 -0700

    Updates

---
+++ precache-manifest.8915f66a9248eeb4f312881d6c90fca0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2f546fd0bd413700d3b4f908c869a2",
+    "url": "/Unsplash-Api/index.html"
+  },
+  {
+    "revision": "b5a970f291e65376af99",
+    "url": "/Unsplash-Api/static/css/main.ae6466ce.chunk.css"
+  },
+  {
+    "revision": "97f81f4095946ed53456",
+    "url": "/Unsplash-Api/static/js/2.136894e0.chunk.js"
+  },
+  {
+    "revision": "b5a970f291e65376af99",
+    "url": "/Unsplash-Api/static/js/main.83fcef0c.chunk.js"
+  },
+  {
+    "revision": "1f24c24f69b73be9c89f",
+    "url": "/Unsplash-Api/static/js/runtime~main.7c4f0f94.js"
+  }
+]);
